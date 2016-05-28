45277814998d8606fd42afa5b9170226e1ca4be6
Olivier Berthonneau <berthonneau.olivier@gmail.com>
2016-05-28 23:58:50 +0200

    Add real time metrics and remove water statistic

--- front/app/controllers/dashboard.js
+++ front/app/controllers/dashboard.js
@@ -4,20 +4,29 @@ export default Ember.Controller.extend({
 
   application: Ember.inject.controller(),
 
+  truncate: function(num, places) {
+    return Math.trunc(num * Math.pow(10, places)) / Math.pow(10, places);
+  },
+
   totalNumber: 0, // There is always at least one greener (you)
-  totalWater: 0,
-  totalElectricity: 0,
-  totalCO2: 0,
+  totalElectricity: function() {
+    return this.truncate(this.get('totalNumber') * (346 / 365 / 24), 2);
+  }.property('totalNumber'),
+
+  totalCO2: function() {
+    return this.truncate(this.get('totalNumber') * (203 / 365 / 24), 2);
+  }.property('totalNumber'),
 
   schedulePing: function() {
 
     Ember.run.later((() => {
 
       let currentUser = this.get('application.model');
-      currentUser.set('updatedAt', new Date());
 
+      currentUser.set('updatedAt', new Date());
       currentUser.save();
       this.schedulePing();
+
     }), 1000);
 
   }.on('init')
