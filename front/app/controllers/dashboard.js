a6028534dd29ef508d21c0e5668399e3165ef829
Olivier Berthonneau <berthonneau.olivier@gmail.com>
2016-05-29 01:13:39 +0200

    Add background stats

--- front/app/controllers/dashboard.js
+++ front/app/controllers/dashboard.js
@@ -18,8 +18,29 @@ export default Ember.Controller.extend({
     return this.truncate(this.get('totalNumber') * (203 / 365 / 24), 2);
   }.property('totalNumber'),
 
+  currentIteration: 0,
+  stats: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+
+  updateStats: function() {
+
+    let current = this.get('currentIteration');
+    this.get('stats')[current] = this.get('totalNumber');
+
+    for (let i = 0; i < 10; i++) {
+
+      if (current === 10) {
+	current = 0;
+      }
+
+      Ember.$(".bar" + current).children('.grow').css('height', (this.get('stats.' + current) * 10) + "%");
+      current++;
+    }
+
+  }.observes('currentIteration'),
+
   schedulePing: function() {
 
+    this.updateStats();
     Ember.run.later((() => {
 
       let currentUser = this.get('application.model');
@@ -27,6 +48,15 @@ export default Ember.Controller.extend({
       currentUser.set('updatedAt', new Date());
       currentUser.save();
       this.schedulePing();
+
+      var current = this.get('currentIteration');
+      if (current === 9) {
+	current = 0;
+      } else {
+	current = current + 1;
+      }
+
+      this.set('currentIteration', current);
 
     }), 1000);
 
