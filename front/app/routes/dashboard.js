17cd5e497fe05934f32522725c8200aa14eacd67
Olivier Berthonneau <berthonneau.olivier@gmail.com>
2016-05-28 20:02:35 +0200

    Filter users to only get recently connected user

--- front/app/routes/dashboard.js
+++ front/app/routes/dashboard.js
@@ -2,7 +2,25 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
 
+  ALLOWED_IDLE: 5,
+
   model: function() {
-    return this.store.findAll('user');
+    return this.store.findAll('user')
+      .then((users) => {
+
+	var recentUser = [];
+
+	users.forEach((user) => {
+
+	  let time = new Date();
+	  time.setSeconds(time.getSeconds() - this.ALLOWED_IDLE);
+
+	  if (user.get('updatedAt') > time) {
+	    recentUser.push(user);
+	  }
+	});
+
+	return recentUser;
+      });
   }
 });
