fb001452d325d8059a16463d9df166fadd91ca5f
Olivier Berthonneau <berthonneau.olivier@gmail.com>
2016-05-28 21:37:48 +0200

    Schedule model refresh every second

--- front/app/routes/dashboard.js
+++ front/app/routes/dashboard.js
@@ -3,6 +3,16 @@ import Ember from 'ember';
 export default Ember.Route.extend({
 
   ALLOWED_IDLE: 5,
+  REFRESH_INTERVAL: 1000, // Every one second
+
+  scheduleRefresh: function() {
+
+    setTimeout(() => {
+      this.refresh();
+    }, this.REFRESH_INTERVAL);
+
+  },
+
 
   model: function() {
     return this.store.findAll('user')
@@ -20,6 +30,7 @@ export default Ember.Route.extend({
 	  }
 	});
 
+	this.scheduleRefresh();
 	return recentUser;
       });
   }
